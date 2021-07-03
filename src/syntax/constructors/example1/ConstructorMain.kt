c13b160abfc21951383072b74b96d69a496ad9ae
soaedu <sergey.ostrovsky.edu@gmail.com>
2021-07-03 19:21:20 +0300

    Add new primary and secondary constructor examples.

--- src/syntax/constructors/example1/ConstructorMain.kt
+++ src/syntax/constructors/example1/ConstructorMain.kt
@@ -1,17 +1,27 @@
 package syntax.constructors.example1
 
-
-fun main(args: Array<String>) {
+fun main() {
     val human1 = Human1()
     human1.firstName = "Gavrila Hlebnikov"
     human1.age = 27
-    println("\nHuman1 info: \n\tname: ${human1.firstName} \n\tage: ${human1.age}")
+    println("\n1. Human1 info: " +
+            "\n\tname: ${human1.firstName}" +
+            "\n\tage: ${human1.age}"
+    )
 
     val human2 = Human2("Ivan Petrov", 25)
-    println("\nHuman2 info: \n\tname: ${human2.firstName} \n\tage: ${human2.age} \n\tmessage: ${human2.message}")
+    println("\n2. Human info: " +
+            "\n\tname: ${human2.firstName}" +
+            "\n\tage: ${human2.age}" +
+            "\n\tmessage: ${human2.message}"
+    )
 
     val human3 = Human2()
-    println("\nHuman3 info: \n\tname: ${human3.firstName} \n\tage: ${human3.age} \n\tmessage: ${human3.message}")
+    println("\n3. Human info: " +
+            "\n\tname: ${human3.firstName}" +
+            "\n\tage: ${human3.age}" +
+            "\n\tmessage: ${human3.message}"
+    )
 }
 
 // primary constructor with annotation
@@ -32,7 +42,8 @@ class Human1 {
 
 // #1
 /* class Human2(firstName: String, var age: Int) {
-    val firstName = firstName */
+    val firstName = firstName
+*/
 
 // #2
 class Human2(val firstName: String, var age: Int) {
@@ -40,7 +51,5 @@ class Human2(val firstName: String, var age: Int) {
 
     // secondary constructor
     constructor(name: String, age: Int, msg:String): this(name, age)
-
-    // secondary constructor
     constructor(): this("No name", 0)
 }
